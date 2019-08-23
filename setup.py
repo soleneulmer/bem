e97d5eabb7f9b397e36bf8648c4be6fb21a47aee
Solène <solene.ulmer-moll@astro.up.pt>
2019-08-23 16:57:39 +0100

    update version to 0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 output = os.path.join(here, 'published_output/')
 
 setup(name='bem',
-      version='0.1.8',
+      version='0.1.9',
       description='Random forest for exoplanets',
       long_description=open("README.md").read(),
       long_description_content_type='text/markdown',
